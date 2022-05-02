a399f2fdda7c7f11c9f6b4fbcf124cd3cab6b5dd
mclilzee <mc.lilzee@hotmail.com>
2022-05-02 20:11:37 +0200

    Create dice and functionality

---
+++ src/components/Dice.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Dice(props) {
+
+    function handleButtonClick() {
+        props.selectDice(props.index);
+    }
+
+    return (
+        <div className={`dice ${props.isSelected ? "selected" : ""}`} onClick={handleButtonClick}>{props.number}</div>
+    )
+}
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,52 @@
 import React from "react";
+import Dice from "./Dice"
 
 export default function Main(props) {
-    return <h1>Test</h1>
+
+    const [diceStates, setDiceStates] = React.useState(() => {
+        return createTable(6)
+    })
+
+    function generateRandomNumber(number) {
+        return Math.floor(Math.random() * number);
+    }
+
+    function selectDice(index) {
+
+        setDiceStates(prevArray => {
+           const newArray = [...prevArray];
+           newArray[index].isSelected = !prevArray[index].isSelected;
+           return newArray;
+        })
+    }
+
+    function createTable(size) {
+        const table = []
+        for (let i = 0; i < size; i++) {
+            const dice = {
+                number: generateRandomNumber(6),
+                isSelected: false
+            }
+
+            table.push(dice);
+        }
+
+        return table;
+    }
+
+    const diceTable = diceStates.map((diceItem, index) => {
+        return <Dice index={index}
+                     key={index}
+                     selectDice={selectDice}
+                     isSelected={diceItem.isSelected}
+                     number={diceItem.number}
+        />
+    })
+
+
+    return (
+        <div className="diceContainer">
+            {diceTable}
+        </div>
+    )
 }
