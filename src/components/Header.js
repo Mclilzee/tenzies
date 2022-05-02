43f2601d53854f10308f2522b9e9f6dacd90a70a
mclilzee <mc.lilzee@hotmail.com>
2022-05-02 17:00:40 +0200

    Setup basic layout

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header(props) {
+    return (
+        <div className="header">
+            <h3>Tenzies</h3>
+            <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        </div>
+    )
+}
